aba5dc4db0acd403221ba1644cd52b9482ca844c
Andrei Itiligator <a.e.itiligator@gmail.com>
2023-03-28 23:03:53 +0700

    ref: move to gtest, remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
-#include "../include/metainfo.h"
+#include "somelib.h"
 
 #include <iostream>
 
 int main(int, char **) {
-	std::cout << "Version: " << patch_number() << std::endl;
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
---
+++ tests/gtest.cc
@@ -0,0 +1,5 @@
+//
+// Created by andrei on 10.03.23.
+//
+
+#include <gtest/gtest.h>
